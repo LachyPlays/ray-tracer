39a603220e0fc5fce3f5aff9861137861144b942
Lachlan Munday <munday.ljm@gmail.com>
2022-11-07 20:29:34 +1100

    all the stuff

--- public/index.js
+++ public/index.js
@@ -1,15 +1,55 @@
+// Global canvas variables
+var frame;
+var render_context;
+var width;
+var height;
+var frame_data;
+var buffer;
+var buf_u8;
+var framebuffer;
+// Make an RGBA pixel into a Number
+var makeRGBA = function (r, g, b, a) {
+    return ((a << 24) | (b << 16) | (g << 8) | (r << 0));
+};
 // Functions called from index.html
-function canvasLoaded() {
-    console.log("Canvas loaded");
-}
-function renderFrame() {
-    var frame = document.getElementById("frame");
-    // If canvas found begin
-    if (frame) {
-        var render_context = frame.getContext('2d');
-        render_context.fillStyle = "#000000";
-        render_context === null || render_context === void 0 ? void 0 : render_context.fillRect(0, 0, frame.width, frame.height);
-    }
+var loaded = function () {
+    frame = document.getElementById("frame");
+    render_context = frame.getContext('2d');
+    width = frame.width;
+    height = frame.height;
+    frame_data = render_context.createImageData(width, height);
+    buffer = new ArrayBuffer(frame_data.data.length);
+    buf_u8 = new Uint8ClampedArray(buffer);
+    framebuffer = new Uint32Array(buffer);
+    console.log("Load complete!");
+};
+var renderFrame = function () {
     console.log("Rendering frame...");
+    traceRay();
+    for (var i = 0; i < width * height; i++) {
+        framebuffer[i] = makeRGBA(0x00, 0x00, 0x00, 0xff);
+    }
+    frame_data.data.set(buf_u8);
+    render_context.putImageData(frame_data, 0, 0);
     console.log("Rendered!");
+};
+// Raytracing code
+var traceRay = function () { console.log("Traced a ray!"); };
+// Vector 3 Addition
+function v3Add(a, b) {
+    return { x: a.x + b.x, y: a.y + b.y, z: a.z + b.z };
+}
+// Vector 3 subtraction
+function v3Sub(a, b) {
+    return { x: a.x - b.x, y: a.y - b.y, z: a.z - b.z };
+}
+function v3Mul(a, b) {
+    if (typeof b == 'number') {
+        // If arg b is a number multiply vector * scalar
+        return { x: a.x * b, y: a.y * b, z: a.z * b };
+    }
+    else {
+        // Else is arg b is Vec3 multiply vector * vector
+        return { x: a.x * b.x, y: a.y * b.y, z: a.z * b.z };
+    }
 }
--- public/index.ts
+++ public/index.ts
@@ -1,19 +1,83 @@
-// Functions called from index.html
-function canvasLoaded() {
-    console.log("Canvas loaded")
+// Global canvas variables
+var frame: HTMLCanvasElement
+var render_context: CanvasRenderingContext2D
+var width: number
+var height: number
+var frame_data: ImageData
+var buffer: ArrayBuffer
+var buf_u8: Uint8ClampedArray
+var framebuffer: Uint32Array
+
+// Make an RGBA pixel into a Number
+const makeRGBA = (r: number, g: number, b: number, a: number) => {
+    return ((a << 24) | (b << 16) | (g << 8) | (r << 0))
 }
 
-function renderFrame() {
-    let frame = document.getElementById("frame") as HTMLCanvasElement | null
+// Functions called from index.html
+const loaded = () => {
+    frame = document.getElementById("frame") as HTMLCanvasElement
+    render_context = frame.getContext('2d') as CanvasRenderingContext2D
+    width = frame.width
+    height = frame.height
+    frame_data = render_context.createImageData(width, height)
+    buffer = new ArrayBuffer(frame_data.data.length)
+    buf_u8 = new Uint8ClampedArray(buffer)
+    framebuffer = new Uint32Array(buffer)
 
-    // If canvas found begin
-    if(frame){
-        const render_context = frame.getContext('2d') as CanvasRenderingContext2D
-        render_context.fillStyle = "#000000"
+    console.log("Load complete!")
+}
 
-        render_context?.fillRect(0, 0, frame.width, frame.height)
+const renderFrame = () => {
+    console.log("Rendering frame...")
+
+    traceRay()
+
+    for (let i = 0; i < width * height; i++) {
+        framebuffer[i] = makeRGBA(0x00, 0x00, 0x00, 0xff)
     }
 
-    console.log("Rendering frame...")
+    frame_data.data.set(buf_u8)
+    render_context.putImageData(frame_data, 0, 0)
+
     console.log("Rendered!")
 }
+
+// Raytracing code
+const traceRay = () => {console.log("Traced a ray!")}
+
+// Vector 3
+interface Vec3 {
+    x: number,
+    y: number,
+    z: number
+}
+
+// Vector 3 Addition
+function v3Add(a: Vec3, b: Vec3) : Vec3 {
+    return {x: a.x + b.x, y: a.y + b.y, z: a.z + b.z} 
+}
+
+// Vector 3 subtraction
+function v3Sub(a: Vec3, b: Vec3) : Vec3 {
+    return {x: a.x - b.x, y: a.y - b.y, z: a.z - b.z} 
+}
+
+// Vector 3 multiply
+function v3Mul(a: Vec3, b: Vec3) : Vec3;
+function v3Mul(a: Vec3, b: number) : Vec3
+
+function v3Mul(a: Vec3, b: Vec3 | number) : Vec3 {
+    if(typeof b == 'number'){
+        // If arg b is a number multiply vector * scalar
+        return {x: a.x * b, y: a.y * b, z: a.z * b} 
+    } else {
+        // Else is arg b is Vec3 multiply vector * vector
+        return {x: a.x * b.x, y: a.y * b.y, z: a.z * b.z} 
+    }
+}
+
+// Vector 3 dot-product
+function v3Dot(a: Vec3, b: Vec3): number {
+    return (a.x * b.y) + (a.y * b.y) + (a.z * b.z)
+}
+
